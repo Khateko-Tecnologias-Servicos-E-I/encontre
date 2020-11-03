fd068cde2ecaaa58baa6430bdb09d8eaeff27247
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2020-11-03 23:54:22 +0200

    Home and welcome litle translations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Esta logado.') }}
                 </div>
             </div>
         </div>
