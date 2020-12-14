dbdbb550294f0eda109d44c1b2d5c2da0b13bd20
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2020-12-14 10:35:49 +0200

    Update Sign in page

--- resources/views/entrada.blade.php
+++ resources/views/entrada.blade.php
@@ -8,7 +8,7 @@
         <meta name="description" content="Sleek Dashboard - Free Bootstrap 4 Admin Dashboard Template and UI Kit. It is very powerful bootstrap admin dashboard, which allows you to build products like admin panels, content management systems and CRMs etc.">
 
 
-        <title></title>
+        <title>Encontre - Tudo que procura! | Entrada</title>
 
         <!-- GOOGLE FONTS -->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet" />
@@ -48,7 +48,7 @@
             <div class="card">
                 <div class="card-header bg-primary">
                     <div class="app-brand">
-                        <a href="/index.html">
+                        <a href="/">
                             <svg class="brand-icon" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="30" height="33"
                                  viewBox="0 0 30 33">
                                 <g fill="none" fill-rule="evenodd">
@@ -56,35 +56,35 @@
                                     <path class="logo-fill-white" fill="#FFF" d="M11 4v25l8 4V0z" />
                                 </g>
                             </svg>
-                            <span class="brand-name">Sleek Dashboard</span>
+                            <span class="brand-name">Voltar</span>
                         </a>
                     </div>
                 </div>
                 <div class="card-body p-5">
 
-                    <h4 class="text-dark mb-5">Sign In</h4>
+                    <h4 class="text-dark mb-5">Entrada</h4>
                     <form action="/index.html">
                         <div class="row">
                             <div class="form-group col-md-12 mb-4">
-                                <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" placeholder="Username">
+                                <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" placeholder="Nome de utilizador">
                             </div>
                             <div class="form-group col-md-12 ">
-                                <input type="password" class="form-control input-lg" id="password" placeholder="Password">
+                                <input type="password" class="form-control input-lg" id="password" placeholder="Senha">
                             </div>
                             <div class="col-md-12">
                                 <div class="d-flex my-2 justify-content-between">
                                     <div class="d-inline-block mr-3">
-                                        <label class="control control-checkbox">Remember me
+                                        <label class="control control-checkbox">Lembrar de mim
                                             <input type="checkbox" />
                                             <div class="control-indicator"></div>
                                         </label>
 
                                     </div>
-                                    <p><a class="text-blue" href="#">Forgot Your Password?</a></p>
+                                    <p><a class="text-blue" href="#">Esqueceu a senha?</a></p>
                                 </div>
-                                <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Sign In</button>
-                                <p>Don't have an account yet ?
-                                    <a class="text-blue" href="sign-up.html">Sign Up</a>
+                                <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Entrar</button>
+                                <p>Não possui conta ?
+                                    <a class="text-blue" href="sign-up.html">Regista-se</a>
                                 </p>
                             </div>
                         </div>
@@ -94,8 +94,8 @@
         </div>
     </div>
     <div class="copyright pl-0">
-        <p class="text-center">&copy; 2018 Copyright Sleek Dashboard Bootstrap Template by
-            <a class="text-primary" href="http://www.iamabdus.com/" target="_blank">Abdus</a>.
+        <p class="text-center">&copy; 2021 Encontre. Um produto da
+            <a class="text-primary" href="http://www.khateko.com/" target="_blank">Khateko</a>.
         </p>
     </div>
 </div>
