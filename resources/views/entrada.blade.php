3ec4f2fc6eb3daf53004e2ca9ae3765c709681d7
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2020-12-17 22:06:02 +0200

    Update entrada.blade.php

--- resources/views/entrada.blade.php
+++ resources/views/entrada.blade.php
@@ -16,12 +16,12 @@
 
 
         <!-- PLUGINS CSS STYLE -->
-        <link href="{{ asset('assets/plugins/back-end/nprogress/nprogress.css') }}" rel="stylesheet" />
+        <link href="{{ asset('assets/plugins//nprogress/nprogress.css') }}" rel="stylesheet" />
 
 
 
         <!-- SLEEK CSS -->
-        <link id="sleek-css" rel="stylesheet" href="{{ asset('assets/css/back-end/sleek.css') }}" />
+        <link id="sleek-css" rel="stylesheet" href="{{ asset('assets/css/sleek.css') }}" />
 
         <!-- FAVICON -->
         <link href="{{ asset('assets/img/back-end/favicon.png') }}" rel="shortcut icon" />
@@ -36,7 +36,7 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <script src="{{ asset('assets/plugins/back-end/nprogress/nprogress.js') }}"></script>
+        <script src="{{ asset('assets/plugins/nprogress/nprogress.js') }}"></script>
     </head>
 
 </head>
